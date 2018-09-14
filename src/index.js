b8d1f2337c104fbaef31fc4d9fa5b7a24aa0c047
Erzender <lukas.parant@epitech.eu>
2018-09-14 15:41:37 +0200

    basic setup

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/* eslint-disable */
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+/* eslint-disable */
