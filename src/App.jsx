d3d734fe19f7eb2702eaa9aeae5028119c36afd8
Erzender <lukas.parnat@epitech.eu>
2018-10-21 11:12:52 +0200

    line breaks issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
-
 import Router from './views/Router';
 
 const store = createStore(compose(applyMiddleware(thunkMiddleware)));
-
 const App = () => (
   <Provider store={store}>
+    {' '}
     <Router />
+    {' '}
   </Provider>
 );
-
 export default App;
